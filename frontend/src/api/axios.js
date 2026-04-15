5759bfcd9c5e0c8df93051956044724cefdda71a
Balu Pasumarthi <balupasumarthi1@gmail.com>
2026-04-15 07:42:07 +0530

    updated server url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://purplemerit-n9z1.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
